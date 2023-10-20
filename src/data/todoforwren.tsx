ac91061cb3cbc189269174b7f4ad121b98fadec7
HRSPA <hrsparro@gmail.com>
2023-10-20 14:28:57 -0600

    Timing Cleaning 1

---
+++ src/data/todoforwren.tsx
@@ -0,0 +1,99 @@
+
+
+export const femaleEndingOneTextTiming =[
+    {id:1, speaker:"investigator", text:"Yes, you do deserve further investigating at least. ",enterTime: 0, exitTime: 0},
+    {id:2, speaker:"investigator", text:"Are you going to come with me quietly or will you make a fuss? ",enterTime: 0, exitTime: 0},
+    {id:3, speaker:"weaver", text:"What would you do if I did?",enterTime: 0, exitTime: 0},
+    {id:4, speaker:"investigator", text:"I’ve been authorized to use deadly force if needed. So?",enterTime: 0, exitTime: 0},
+    {id:5, speaker:"investigator", text:"So?",enterTime: 0, exitTime: 0},   
+    {id:6, speaker:"weaver", text:"Alright. I’ll go quietly. ",enterTime: 0, exitTime: 0},
+    {id:7, speaker:"investigator", text:"Good. Let’s go.",enterTime: 0, exitTime: 0},
+    {id:8, speaker:"weaver", text:"Will I be able to contact my family?",enterTime: 0, exitTime: 0},
+    {id:9, speaker:"investigator", text:"Yes, eventually.",enterTime: 0, exitTime: 0},
+    {id:10, speaker:"narrator", text:"You begin to move out of the room",enterTime: 0, exitTime: 0},
+    {id:11, speaker:"narrator", text:"The weaver follows you, looking around their workshop.",enterTime: 0, exitTime: 0},
+    {id:12, speaker:"narrator", text:"You remember to take out an evidence bag. ",enterTime: 0, exitTime: 0},
+    {id:13, speaker:"narrator", text:"You don’t want to touch the carpet squares, but you need to take one.",enterTime: 0, exitTime: 0},
+    {id:14, speaker:"weaver", text:"Do you think they’ll let me weave a silk scarf in jail? I haven’t done one in ages.",enterTime: 0, exitTime: 0},
+    {id:15, speaker:"narrator", text:"You don’t know what to say, so you say nothing.",enterTime: 0, exitTime: 0},
+    {id:16, speaker:"narrator", text:"You remember the handcuffs you were given. You take them out and the weaver lets you put them on.",enterTime: 0, exitTime: 0},
+    {id:17, speaker:"narrator", text: "You pick up the evidence bag filled with one carpet square.",enterTime: 0, exitTime: 0},
+    {id:18, speaker:"narrator", text: "You both move down the long dark hallway into the grey afternoon.",enterTime: 0, exitTime: 0},
+    {id:1007, speaker:"choice", text: "What now?",enterTime: 0, exitTime: 0},
+]
+
+export const maleEndingOneTextTiming =[
+    {id:1, speaker:"investigator", text:"Yes, you do deserve further investigating at least. ",enterTime: 0, exitTime: 0},
+    {id:2, speaker:"investigator", text:"Are you going to come with me quietly or will you make a fuss? ",enterTime: 0, exitTime: 0},
+    {id:3, speaker:"weaver", text:"What would you do if I did?",enterTime: 0, exitTime: 0},
+    {id:4, speaker:"investigator", text:"I’ve been authorized to use deadly force if needed. So?",enterTime: 0, exitTime: 0},
+    {id:5, speaker:"investigator", text:"So?",enterTime: 0, exitTime: 0},   
+    {id:6, speaker:"weaver", text:"Alright. I’ll go quietly. ",enterTime: 0, exitTime: 0},
+    {id:7, speaker:"investigator", text:"Good. Let’s go.",enterTime: 0, exitTime: 0},
+    {id:8, speaker:"weaver", text:"Will I be able to contact my family?",enterTime: 0, exitTime: 0},
+    {id:9, speaker:"investigator", text:"Yes, eventually.",enterTime: 0, exitTime: 0},
+    {id:10, speaker:"narrator", text:"You begin to move out of the room",enterTime: 0, exitTime: 0},
+    {id:11, speaker:"narrator", text:"The weaver follows you, looking around their workshop.",enterTime: 0, exitTime: 0},
+    {id:12, speaker:"narrator", text:"You remember to take out an evidence bag. ",enterTime: 0, exitTime: 0},
+    {id:13, speaker:"narrator", text:"You don’t want to touch the carpet squares, but you need to take one.",enterTime: 0, exitTime: 0},
+    {id:14, speaker:"weaver", text:"Do you think they’ll let me weave a silk scarf in jail? I haven’t done one in ages.",enterTime: 0, exitTime: 0},
+    {id:15, speaker:"narrator", text:"You don’t know what to say, so you say nothing.",enterTime: 0, exitTime: 0},
+    {id:16, speaker:"narrator", text:"You remember the handcuffs you were given. You take them out and the weaver lets you put them on.",enterTime: 0, exitTime: 0},
+    {id:17, speaker:"narrator", text: "You pick up the evidence bag filled with one carpet square.",enterTime: 0, exitTime: 0},
+    {id:18, speaker:"narrator", text: "You both move down the long dark hallway into the grey afternoon.",enterTime: 0, exitTime: 0},
+    {id:1007, speaker:"choice", text: "What now?",enterTime: 0, exitTime: 0},
+]
+
+export const nonBinaryEndingOneTextTiming =[
+    {id:1, speaker:"investigator", text:"Yes, you do deserve further investigating at least. ",enterTime: 0, exitTime: 0},
+    {id:2, speaker:"investigator", text:"Are you going to come with me quietly or will you make a fuss? ",enterTime: 0, exitTime: 0},
+    {id:3, speaker:"weaver", text:"What would you do if I did?",enterTime: 0, exitTime: 0},
+    {id:4, speaker:"investigator", text:"I’ve been authorized to use deadly force if needed. So?",enterTime: 0, exitTime: 0},
+    {id:5, speaker:"investigator", text:"So?",enterTime: 0, exitTime: 0},   
+    {id:6, speaker:"weaver", text:"Alright. I’ll go quietly. ",enterTime: 0, exitTime: 0},
+    {id:7, speaker:"investigator", text:"Good. Let’s go.",enterTime: 0, exitTime: 0},
+    {id:8, speaker:"weaver", text:"Will I be able to contact my family?",enterTime: 0, exitTime: 0},
+    {id:9, speaker:"investigator", text:"Yes, eventually.",enterTime: 0, exitTime: 0},
+    {id:10, speaker:"narrator", text:"You begin to move out of the room",enterTime: 0, exitTime: 0},
+    {id:11, speaker:"narrator", text:"The weaver follows you, looking around their workshop.",enterTime: 0, exitTime: 0},
+    {id:12, speaker:"narrator", text:"You remember to take out an evidence bag. ",enterTime: 0, exitTime: 0},
+    {id:13, speaker:"narrator", text:"You don’t want to touch the carpet squares, but you need to take one.",enterTime: 0, exitTime: 0},
+    {id:14, speaker:"weaver", text:"Do you think they’ll let me weave a silk scarf in jail? I haven’t done one in ages.",enterTime: 0, exitTime: 0},
+    {id:15, speaker:"narrator", text:"You don’t know what to say, so you say nothing.",enterTime: 0, exitTime: 0},
+    {id:16, speaker:"narrator", text:"You remember the handcuffs you were given. You take them out and the weaver lets you put them on.",enterTime: 0, exitTime: 0},
+    {id:17, speaker:"narrator", text: "You pick up the evidence bag filled with one carpet square.",enterTime: 0, exitTime: 0},
+    {id:18, speaker:"narrator", text: "You both move down the long dark hallway into the grey afternoon.",enterTime: 0, exitTime: 0},
+    {id:1007, speaker:"choice", text: "What now?",enterTime: 0, exitTime: 0},
+]
+
+export const EndingTwoATextTiming =[
+    {id:1, speaker:"narrator", text:"You look at your watch. ",enterTime: 0, exitTime: 0},
+    {id:2, speaker:"narrator", text:"This took longer than expected. You’ll try to keep it shorter next time.",enterTime: 0, exitTime: 0},
+    {id:3, speaker:"narrator", text:"You remember to take out an evidence bag. ",enterTime: 0, exitTime: 0},
+    {id:4, speaker:"narrator", text:"You don’t want to touch the carpet squares, but you need to take one.",enterTime: 0, exitTime: 0},
+    {id:5, speaker:"narrator", text:"Even if you let the weaver go, this is still evidence of the whole operation.",enterTime: 0, exitTime: 0},
+
+   
+    {id:6, speaker:"narrator", text:"That done, you look up the next address. ",enterTime: 0, exitTime: 0},
+    {id:7, speaker:"narrator", text:"You leave as quickly as you can.",enterTime: 0, exitTime: 0},
+    {id:7777, speaker:"choice", text:"What now?",enterTime: 0, exitTime: 0},
+]
+ 
+export const EndingTwoBTextTiming =[
+    {id:1, speaker:"narrator", text:"You look at your watch.",enterTime: 0, exitTime: 0},
+    {id:2, speaker:"narrator", text:"You might have to finally sell it if you go through with this and can’t find another job. ",enterTime: 0, exitTime: 0},
+    {id:3, speaker:"narrator", text:"You won’t go to the next address.",enterTime: 0, exitTime: 0},
+    {id:4, speaker:"narrator", text:"You’ll go back to the office and turn in your notice. ",enterTime: 0, exitTime: 0},
+    {id:5, speaker:"narrator", text:"You remember to take out an evidence bag. ",enterTime: 0, exitTime: 0},
+    {id:6, speaker:"narrator", text:"You don’t want to touch the carpet squares, but you need to take one.",enterTime: 0, exitTime: 0},
+    {id:7, speaker:"narrator", text:"Even if you let the weaver go, this is still evidence of the whole operation.",enterTime: 0, exitTime: 0},
+    {id:8, speaker:"narrator", text:"Part of you is worried there will be repercussions, but this isn’t the old government.",enterTime: 0, exitTime: 0},
+    {id:9, speaker:"narrator", text:"They also don’t need to punish you, searching for a job again will be punishment enough.",enterTime: 0, exitTime: 0},
+    {id:10, speaker:"narrator", text:"You begin to leave, but before you do, you think to look around the workshop and weaver’s room for food.",enterTime: 0, exitTime: 0},
+    {id:11, speaker:"narrator", text:"There isn’t much, but you find some bread and a few snacks that will be easy to carry.",enterTime: 0, exitTime: 0},
+    {id:12, speaker:"narrator", text:"Waste not want not.",enterTime: 0, exitTime: 0},
+    {id:13, speaker:"narrator", text:"You begin to choke on the fibre dust.",enterTime: 0, exitTime: 0},
+    {id:14, speaker:"narrator", text:"You leave as quickly as you can.",enterTime: 0, exitTime: 0},
+    {id:1111, speaker:"choice", text:"What now?",enterTime: 0, exitTime: 0},
+
+]
